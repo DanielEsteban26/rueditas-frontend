3db2bd7788249a1e105ecaa6f414fe5642c25f79
Daniel Isidro <isidroesteban402@gmail.com>
2024-09-26 16:10:53 -0500

    creacion de record y puerto

---
+++ src/main/java/pe/edu/cibertec/rueditasfrontend/dto/RueditasResponse.java
@@ -0,0 +1,4 @@
+package pe.edu.cibertec.rueditasfrontend.dto;
+
+public record RueditasResponse(String codigo, String mensaje, String marca, String modelo, String nroAsientos, String precio, String color) {
+}
---
+++ src/main/java/pe/edu/cibertec/rueditasfrontend/viewmodel/RueditaModel.java
@@ -0,0 +1,4 @@
+package pe.edu.cibertec.rueditasfrontend.viewmodel;
+
+public record RueditaModel(String codigo, String mensaje, String marca, String modelo, String nroAsientos, String precio, String color) {
+}
